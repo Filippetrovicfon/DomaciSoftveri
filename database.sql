ad1c825acfa6f57dfee4ee32caf6a58d5c668c0c
Filip Petrovic <57798831+Filippetrovicfon@users.noreply.github.com>
2019-11-15 13:49:47 +0100

    Add files via upload

--- database.sql
+++ database.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Community v13.1.1 (64 bit)
-MySQL - 10.1.35-MariaDB : Database - d2
+MySQL - 10.1.35-MariaDB : Database - d1
 *********************************************************************
 */
 
@@ -12,25 +12,25 @@ MySQL - 10.1.35-MariaDB : Database - d2
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`d2` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`d1` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
 
-USE `d2`;
+USE `d1`;
 
 /*Table structure for table `manufacturer` */
 
 DROP TABLE IF EXISTS `manufacturer`;
 
 CREATE TABLE `manufacturer` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 /*Data for the table `manufacturer` */
 
 insert  into `manufacturer`(`id`,`name`) values 
-(1,'Manufacturer/1'),
-(2,'New name for Manufacturer/2'),
+(1,'Manufacturer 1'),
+(2,'Manufacturer 2'),
 (3,'Manufacturer 3');
 
 /*Table structure for table `product` */
@@ -38,34 +38,36 @@ insert  into `manufacturer`(`id`,`name`) values
 DROP TABLE IF EXISTS `product`;
 
 CREATE TABLE `product` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `description` varchar(250) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `measurment_unit` varchar(3) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `price` decimal(10,0) DEFAULT NULL,
-  `manufacturer_id` bigint(20) unsigned DEFAULT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `description` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `id_manufacturer` bigint(20) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `measurement_unit` varchar(3) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `manufacturer_id` (`manufacturer_id`),
-  CONSTRAINT `product_ibfk_1` FOREIGN KEY (`manufacturer_id`) REFERENCES `manufacturer` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  KEY `id_manufacturer` (`id_manufacturer`),
+  CONSTRAINT `product_ibfk_1` FOREIGN KEY (`id_manufacturer`) REFERENCES `manufacturer` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 /*Data for the table `product` */
 
-insert  into `product`(`id`,`name`,`description`,`measurment_unit`,`price`,`manufacturer_id`) values 
-(1,'producit','aef','KG',120,3),
-(2,'product45','aegr','L',152,1);
+insert  into `product`(`id`,`name`,`description`,`id_manufacturer`,`price`,`measurement_unit`) values 
+(1,'Prod1','desc1',1,100.00,'KG'),
+(2,'Prod2','desc2',2,299.99,'PCS'),
+(3,'Prod3','desc3',2,199.99,'KG'),
+(4,'Prod4','desc4',3,175.99,'PCS');
 
 /*Table structure for table `user` */
 
 DROP TABLE IF EXISTS `user`;
 
 CREATE TABLE `user` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `password` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `firstname` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `lastname` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `email` varchar(150) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `lastname` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `username` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
